e1fe66c9e9f334f1f8eabfed04db0b85e5cfcc87
Nikolay Baraboshkin <starteleport@yandex.ru>
2014-03-22 13:13:52 +0600

    База

---
+++ Tweets/likes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[likes]
+(
+    [userName] VARCHAR(100) NOT NULL, 
+    [messageId] UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES [dbo].[messages]([id]), 
+    [createDate] DATETIME NULL, 
+    PRIMARY KEY ([messageId], [userName])
+)
---
+++ Tweets/messages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[messages]
+(
+	[Id] uniqueidentifier NOT NULL PRIMARY KEY, 
+    [userName] VARCHAR(100) NULL, 
+    [text] VARCHAR(1000) NULL, 
+    [createDate] DATETIME NULL, 
+    [version] ROWVERSION NOT NULL
+)
