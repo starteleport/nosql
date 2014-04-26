183839bc72e4416c183288977ee1b018f9afefe7
Nikolay Baraboshkin <starteleport@yandex.ru>
2014-04-27 05:20:11 +0600

    Сообщения - всё, кроме агрегации

--- Tweets/Tweets/Repositories/MessageRepository.cs
+++ Tweets/Tweets/Repositories/MessageRepository.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using MongoDB.Driver;
 using Tweets.ModelBuilding;
 using Tweets.Models;
 
@@ -25,18 +26,29 @@ namespace Tweets.Repositories
         public void Save(Message message)
         {
             var messageDocument = messageDocumentMapper.Map(message);
-            //TODO: Здесь нужно реализовать вставку сообщения в базу
+            messagesCollection.Insert(messageDocument);
         }
 
         public void Like(Guid messageId, User user)
         {
-            var likeDocument = new LikeDocument {UserName = user.Name, CreateDate = DateTime.UtcNow};
-            //TODO: Здесь нужно реализовать вставку одобрения в базу
+            var message = messagesCollection.FindOne(Query<MessageDocument>.EQ(d => d.Id, messageId));
+
+            if (message != null && message.Likes.All(l => l.UserName != user.Name))
+            {
+                message.Likes = message.Likes.Concat(new [] {new LikeDocument {CreateDate = DateTime.UtcNow, UserName = user.Name}});
+                messagesCollection.Save(message);
+            }
         }
 
         public void Dislike(Guid messageId, User user)
         {
-            //TODO: Здесь нужно реализовать удаление одобрения из базы
+            var message = messagesCollection.FindOne(Query<MessageDocument>.EQ(d => d.Id, messageId));
+            if (message != null)
+            {
+                message.Likes = message.Likes.Where(like => like.UserName != user.Name);
+
+                messagesCollection.Save(message);
+            }
         }
 
         public IEnumerable<Message> GetPopularMessages()
@@ -49,8 +61,19 @@ namespace Tweets.Repositories
 
         public IEnumerable<UserMessage> GetMessages(User user)
         {
-            //TODO: Здесь нужно получать все сообщения конкретного пользователя
-            return Enumerable.Empty<UserMessage>();
+            return
+                messagesCollection.Find(Query<MessageDocument>.EQ(d => d.UserName, user.Name)).SetSortOrder(SortBy<MessageDocument>.Descending(d => d.CreateDate))
+                    .Select(
+                        d =>
+                            new UserMessage
+                            {
+                                CreateDate = d.CreateDate,
+                                Id = d.Id,
+                                Likes = d.Likes == null? 0 : d.Likes.Count(),
+                                Text = d.Text,
+                                User = user,
+                                Liked = d.Likes != null && d.Likes.Any(l => l.UserName == user.Name)
+                            });
         }
     }
 }
